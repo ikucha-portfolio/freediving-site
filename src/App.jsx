37905f2c06feb245871850f6b97bd50a722021ef
Ami <amode0429@gmail.com>
2026-04-07 22:17:32 +0900

    ScrollToTopを追加してページ遷移時にスクロール位置をリセット

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop"; // ←これ追加
 import Courses from "./pages/Courses";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Price from "./pages/Price";
 import FAQ from "./pages/FAQ";
-import Contact from "./pages/Contact"; // ←追加
+import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-[#f5f3ef] min-h-screen">
+      <ScrollToTop /> {/* ←使ってる */}
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} /> {/* 修正 */}
+        <Route path="/courses" element={<Courses />} />
         <Route path="/about" element={<About />} />
         <Route path="/price" element={<Price />} />
         <Route path="/faq" element={<FAQ />} />
-        <Route path="/contact" element={<Contact />} /> {/* 追加 */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
