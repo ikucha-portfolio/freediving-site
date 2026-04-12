1ba08e9b8d873675ca380022e8084270b4d37597
Ami <amode0429@gmail.com>
2026-04-12 18:48:53 +0900

    fix: image path and base config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // ←ここ変更🔥
+base: "/freediving-site/",
   plugins: [react()],
 });
